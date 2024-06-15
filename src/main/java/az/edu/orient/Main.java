14c8107e45c19fc0491e8145c25d8d6ac69d1f7d
muradimanbayli <muradimanbayli@gmail.com>
2024-06-15 11:19:45 +0400

    Ruslan's changes

--- src/main/java/az/edu/orient/Main.java
+++ src/main/java/az/edu/orient/Main.java
@@ -3,6 +3,7 @@ package az.edu.orient;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Test Branch");
+        System.out.println("Ruslan");
         System.out.println("Rashid");
     }
 }
