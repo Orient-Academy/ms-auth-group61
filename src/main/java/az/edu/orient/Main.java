c5f6b0144e0a62e71c40ca764b943b2354158d70
muradimanbayli <muradimanbayli@gmail.com>
2024-06-15 11:14:07 +0400

    reset changes

--- src/main/java/az/edu/orient/Main.java
+++ src/main/java/az/edu/orient/Main.java
@@ -3,6 +3,5 @@ package az.edu.orient;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Test Branch");
-        System.out.println("Ruslan");
     }
 }
