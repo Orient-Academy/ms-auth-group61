90eff97a2ab375f41fce35787b5612ce44823422
muradimanbayli <muradimanbayli@gmail.com>
2024-06-15 10:57:52 +0400

    Rashid changes

--- src/main/java/az/edu/orient/Main.java
+++ src/main/java/az/edu/orient/Main.java
@@ -3,5 +3,6 @@ package az.edu.orient;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Test Branch");
+        System.out.println("Rashid");
     }
 }
